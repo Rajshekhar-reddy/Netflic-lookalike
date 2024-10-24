5dfde2109c6c78f66dbe9821e918a4ff6c6a17ab
E Rajshekhar Reddy <152356450+Rajshekhar-reddy@users.noreply.github.com>
2024-10-24 10:40:44 +0530

    Add files via upload

---
+++ medias/system.js
@@ -0,0 +1,10 @@
+let a = prompt("Enter your Name")
+
+let b =prompt("Enter your Age")
+if (b>17) {
+    alert (a +"is eligible for voting")
+    
+} else {
+    alert (a + " is not eligible for voting")
+    
+}
